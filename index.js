ebecd1e7b0cf3ae93205672f9704b26851de4f0c
Antonio Vieira <amsmv11@gmail.com>
2021-06-20 17:46:47 +0000

    safe state

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const setupRTMP = () => {
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
         // Prod
-        {
+        /*{
           urls: "turn:34.243.91.76:3478",
           username: "eaglestream",
           credential: "root",
-        },
+        },*/
         // Staging
         {
           urls: "turn:3.249.123.75:3478",
@@ -19,35 +19,35 @@ const setupRTMP = () => {
 
     let pc = new RTCPeerConnection(configuration);
     pc.ontrack = function (event) {
-      let el = document.getElementById("video_display");
-      setStreamRef(event.track);
+      // let el = document.createElement(event.track.kind);
+      console.error(event)
+      let el = document.getElementById("video-display");  
       el.srcObject = event.streams[0];
       el.autoplay = true;
       el.controls = true;
-
-    //   el.onloadeddata = function () {
-    //     setStreaming(true);
-    //   };
-
-    //   const endStream = function () {
-    //     setStreaming(false);
-    //   };
-
-    //   event.track.onended = function () {
-    //     endStream();
-    //   };
+      // document.getElementById("rtmpFeed").appendChild(el);  
     };
+
+    // pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
+    // pc.onicecandidate = event => {
+    //   if (event.candidate === null) {
+    //     console.log(btoa(JSON.stringify(pc.localDescription)));  
+    //   }
+    // }
 
     pc.addTransceiver('video')
     pc.addTransceiver('audio')
-    //pc.createOffer({ offerToReceiveVideo: true })
+    // pc.addTransceiver('audio', {'direction': 'recvonly'})
+    // pc.addTransceiver('video', {'direction': 'recvonly'})
+    // pc.addTransceiver('video', {'direction': 'recvonly'})
+
     pc.createOffer()
       .then((offer) => {
         pc.setLocalDescription(offer);
         // let eventID = currentGame.stats.eventId;
 
         return fetch(
-          "http://54.171.42.53/createPeerConnection?eventID=7",
+          "http://54.171.42.53:8080/createPeerConnection?eventID=7",
           {
             method: "post",
             headers: {
@@ -59,8 +59,12 @@ const setupRTMP = () => {
         );
       })
       .then((res) => res.json())
-      .then((res) => pc.setRemoteDescription(res))
+      .then((offer) => pc.setRemoteDescription(offer))
       .catch((err) => console.log(err));
-  };
+};
 
 setupRTMP();
+
+
+
+
